55d6cf8237e8f6b038d912a3bf83ea2bf080ee69
Brian Sherman <bsherman26@hotmail.com>
2012-08-21 16:55:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rd920Tracking::Application.config.secret_token = 'bfcf005fa41a95c55a1abf6eff5d37d18e19de56dd721e63469917a6b06890176a59388ebd62e5c5061f0ed2fe3369b6e95345ae1f55f0f6a007620013d6cb76'
